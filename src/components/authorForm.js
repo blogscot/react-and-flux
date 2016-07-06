5ba5ad0f9d0c165c3374c219e20396df3c9f98c6
blogscot <iain.diamond@gmail.com>
2016-07-06 13:23:22 +0100

    Use Input component

--- src/components/authorForm.js
+++ src/components/authorForm.js
@@ -1,24 +1,28 @@
 const React = require('../../node_modules/react/dist/react')
 
+const Input = require('./textInput')
+
 const AuthorForm = React.createClass({
   render() {
-    const author = this.props.author,
-          onChange = this.props.onChange
     return (
       <div>
         <form>
           <h1>Manage Author</h1>
 
-          <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" className="form-control"
-            placeholder="First Name" ref="firstName"
-            onChange={onChange} value={author.firstName} />
+          <Input
+            name="firstName"
+            label="First Name"
+            placeholder="First Name"
+            value={this.props.author.firstName}
+            onChange={this.props.onChange} />
           <br />
 
-          <label htmlFor="lastName">Last Name</label>
-          <input type="text" name="lastName" className="form-control"
-            placeholder="Last Name" ref="lastName"
-            onChange={onChange} value={author.lastName} />
+          <Input
+            name="lastName"
+            label="Last Name"
+            placeholder="Last Name"
+            value={this.props.author.lastName}
+            onChange={this.props.onChange} />
           <br />
 
           <input type="submit" value="Save" className="btn btn-default" />
