addc60dc0c5de5a4c17cdaf86433ae6b18585a03
blogscot <iain.diamond@gmail.com>
2016-07-06 12:32:14 +0100

    Add change handler

--- src/components/authorForm.js
+++ src/components/authorForm.js
@@ -2,6 +2,8 @@ const React = require('../../node_modules/react/dist/react')
 
 const AuthorForm = React.createClass({
   render() {
+    const author = this.props.author,
+          onChange = this.props.onChange
     return (
       <div>
         <form>
@@ -9,12 +11,14 @@ const AuthorForm = React.createClass({
 
           <label htmlFor="firstName">First Name</label>
           <input type="text" name="firstName" className="form-control"
-            placeholder="First Name" ref="firstName" value="" />
+            placeholder="First Name" ref="firstName"
+            onChange={onChange} value={author.firstName} />
           <br />
 
           <label htmlFor="lastName">Last Name</label>
           <input type="text" name="lastName" className="form-control"
-            placeholder="Last Name" ref="lastName" value="" />
+            placeholder="Last Name" ref="lastName"
+            onChange={onChange} value={author.lastName} />
           <br />
 
           <input type="submit" value="Save" className="btn btn-default" />
--- src/components/manageAuthorPage.js
+++ src/components/manageAuthorPage.js
@@ -2,10 +2,27 @@ const React = require('../../node_modules/react/dist/react')
 const AuthorForm = require('./authorForm')
 
 const ManageAuthorPage = React.createClass({
+  getInitialState() {
+    return {
+      author: {
+        id: '',
+        firstName: '',
+        lastName: ''
+      }
+    }
+  },
+  setAuthorState(event) {
+    const field = event.target.name
+    const value = event.target.value
+    this.state.author[field] = value
+    return this.setState({ author: this.state.author })
+  },
   render() {
     return (
       <div>
-        <AuthorForm />
+        <AuthorForm
+          author={this.state.author}
+          onChange={this.setAuthorState} />
       </div>
     )
   }
